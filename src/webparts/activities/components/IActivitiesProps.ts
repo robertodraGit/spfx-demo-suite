13c106c64635d870e4d74d02209a1faaf5f4a8ce
robertodraGit <rdrago.uni@gmail.com>
2021-11-03 12:00:33 +0100

    activities webpart scaffholding

---
+++ src/webparts/activities/components/IActivitiesProps.ts
@@ -0,0 +1,3 @@
+export interface IActivitiesProps {
+  description: string;
+}
---
+++ src/webparts/activities/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IActivitiesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ActivitiesWebPartStrings' {
+  const strings: IActivitiesWebPartStrings;
+  export = strings;
+}
